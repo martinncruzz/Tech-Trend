4bcad35c7144c35a75baa10d5bdbaf781c0ea722
Martín Cruz <martincruz1426@gmail.com>
2024-06-08 00:37:03 -0300

    [backend] creating db exceptions handler function

---
+++ backend/src/modules/shared/helpers/db-exceptions.handler.ts
@@ -0,0 +1,15 @@
+import {
+  BadRequestException,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+
+export const handleDBExceptions = (error: any, logger: Logger) => {
+  if (error.code === 'P2002')
+    throw new BadRequestException(
+      `Unique constraint violation in ${JSON.stringify(error.meta)}"`,
+    );
+
+  logger.error(error);
+  throw new InternalServerErrorException(`Unexpected error, check server logs`);
+};
