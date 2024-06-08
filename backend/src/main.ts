7ed95b1a5f8e6430a286aabf5b64bfc1b7db423a
Martín Cruz <martincruz1426@gmail.com>
2024-06-08 00:36:28 -0300

    [backend] logger and validation pipe added in the main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(envs.PORT);
+  logger.log(`Server running on port ${envs.PORT}`);
 }
 bootstrap();
