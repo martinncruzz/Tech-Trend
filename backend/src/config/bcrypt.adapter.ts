3a9236095e44ce3b02c39915af18bf46632cd17e
Martín Cruz <martincruz1426@gmail.com>
2024-06-09 01:07:53 -0300

    [backend] implementing bcrypt adapter

---
+++ backend/src/config/bcrypt.adapter.ts
@@ -0,0 +1,8 @@
+import { compareSync, hashSync } from 'bcrypt';
+
+export class BcryptAdapter {
+  static hash = (password: string) => hashSync(password, 10);
+
+  static compare = (password: string, hashedPassword: string) =>
+    compareSync(password, hashedPassword);
+}
