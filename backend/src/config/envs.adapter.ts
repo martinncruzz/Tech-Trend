f7d219730f91b06afdb400c9e076658d35468fd8
Martín Cruz <martincruz1426@gmail.com>
2024-06-09 01:07:38 -0300

    [backend] adding jwt secret and database url envs

--- backend/src/config/envs.adapter.ts
+++ backend/src/config/envs.adapter.ts
@@ -3,11 +3,19 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+
+  JWT_SECRET: string;
+
+  DATABASE_URL: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+
+    JWT_SECRET: joi.string().required(),
+
+    DATABASE_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -19,4 +27,8 @@ const envVars: EnvVars = value;
 
 export const envs = {
   PORT: envVars.PORT,
+
+  JWT_SECRET: envVars.JWT_SECRET,
+
+  DATABASE_URL: envVars.DATABASE_URL,
 };
