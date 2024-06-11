8d7a793845eecb723519710071ad82f4c56ec965
Martín Cruz <martincruz1426@gmail.com>
2024-06-11 17:08:56 -0300

    [frontend] hero-section component for home page added

---
+++ frontend/src/app/pages/home/components/hero-section/hero-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'home-hero-section',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './hero-section.component.html',
+  styles: ``,
+})
+export class HeroSectionComponent {}
