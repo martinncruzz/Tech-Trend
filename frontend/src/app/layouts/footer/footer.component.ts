4d108327bbf56581a0c65000c70462af323fceb5
Martín Cruz <martincruz1426@gmail.com>
2024-06-11 16:59:42 -0300

    [frontend] footer component added

---
+++ frontend/src/app/layouts/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'layouts-footer',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './footer.component.html',
+  styles: ``
+})
+export class FooterComponent {
+
+}
