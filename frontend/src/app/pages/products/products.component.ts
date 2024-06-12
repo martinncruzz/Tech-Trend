91f0feb157d5afbc9acd464ce2a7055db9721be3
Martín Cruz <martincruz1426@gmail.com>
2024-06-12 00:04:38 -0300

    [frontend] products page router implemented to change based in the route

---
+++ frontend/src/app/pages/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './products.component.html',
+  styles: ``,
+})
+export class ProductsComponent {}
