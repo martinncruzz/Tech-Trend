95a864ecde4802dea03cdc880144ceb34d95bc8d
Martín Cruz <martincruz1426@gmail.com>
2024-06-12 00:05:21 -0300

    [frontend] routes for home and products added in global router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./pages/products/products.routes').then((r) => r.PRODUCTS_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
