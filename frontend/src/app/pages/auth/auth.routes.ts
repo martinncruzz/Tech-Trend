3ab4acb93d1db6fc2fdb43eb75e918ac21ff4348
Martín Cruz <martincruz1426@gmail.com>
2024-06-13 02:26:48 -0300

    [frontend] auth page created with its routes

---
+++ frontend/src/app/pages/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+
+export const AUTH_ROUTES: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' },
+];
