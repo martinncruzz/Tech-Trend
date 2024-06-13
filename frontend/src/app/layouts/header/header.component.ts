89951a71598f27cae5f9f4514ac33c6d96afc302
Martín Cruz <martincruz1426@gmail.com>
2024-06-13 02:30:48 -0300

    [frontend] check auth status in the header component added

--- frontend/src/app/layouts/header/header.component.ts
+++ frontend/src/app/layouts/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { AuthService } from '../../core/services';
 
 @Component({
   selector: 'layouts-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styles: ``,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly authService = inject(AuthService);
+
+  public isLoggedIn = computed(() => this.authService.isLoggedIn());
+
+  constructor() {}
+
+  public logout(): void {
+    this.authService.logout();
+  }
+}
