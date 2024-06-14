cf6b58f8293fb0e3320ed5904643c3ae88181c16
Martín Cruz <martincruz1426@gmail.com>
2024-06-14 12:56:55 -0300

    [frontend] admin layout implemented

---
+++ frontend/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'admin-layout',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './admin-layout.component.html',
+  styles: ``,
+})
+export class AdminLayoutComponent {}
