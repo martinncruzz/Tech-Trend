63e1f45e35493aae7ba5ac81b761ef35eb1a1521
Martín Cruz <martincruz1426@gmail.com>
2024-06-16 01:00:58 -0300

    [frontend] fixing routing and sizing of admin layout

--- frontend/src/app/layouts/admin-layout/admin-layout.component.ts
+++ frontend/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'admin-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin-layout.component.html',
   styles: ``,
 })
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent implements OnInit {
+  private readonly router = inject(Router);
+  public currentRoute = signal<string>('');
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.currentRoute.set(this.router.url);
+    });
+  }
+}
