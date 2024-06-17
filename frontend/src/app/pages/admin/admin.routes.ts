3aacdb3ecd1836f0462ad41af97ed3ee8d99a30a
Martín Cruz <martincruz1426@gmail.com>
2024-06-17 12:50:25 -0300

    [frontend] admin routes and dashboard page added

---
+++ frontend/src/app/pages/admin/admin.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const ADMIN_ROUTES: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'dashboard' },
+];
---
+++ frontend/src/app/pages/admin/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styles: ``,
+})
+export class DashboardComponent {}
