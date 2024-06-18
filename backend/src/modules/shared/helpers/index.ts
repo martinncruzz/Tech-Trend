b94e2fca2c519a1eb0111da24e284e6efe51d5fa
Martín Cruz <martincruz1426@gmail.com>
2024-06-18 16:19:13 -0300

    [backend] implementing pagination builder for all modules

--- backend/src/modules/shared/helpers/index.ts
+++ backend/src/modules/shared/helpers/index.ts
@@ -1,1 +1,2 @@
 export * from './db-exceptions.handler';
+export * from './pagination-builder.helper';
---
+++ backend/src/modules/shared/helpers/pagination-builder.helper.ts
@@ -0,0 +1,26 @@
+import { envs } from 'src/config';
+import { PaginationParams, PaginationResponse } from '../interfaces/pagination';
+import { ResourceType } from '../interfaces/pagination/resource-type.enum';
+
+export function getBaseUrl(resourceType: ResourceType): string {
+  return `${envs.BACKEND_URL}/${resourceType}`;
+}
+
+export function buildPaginationResponse<T>({
+  page,
+  limit,
+  total,
+  baseUrl,
+  items,
+}: PaginationParams<T>): PaginationResponse<T> {
+  const next =
+    limit * page >= total ? null : `${baseUrl}?page=${page + 1}&limit=${limit}`;
+  const prev =
+    page - 1 === 0 ? null : `${baseUrl}?page=${page - 1}&limit=${limit}`;
+
+  return {
+    next,
+    prev,
+    items,
+  };
+}
