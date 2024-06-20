252769403532dbdcb131efe9c17e74ac0a74f601
Martín Cruz <martincruz1426@gmail.com>
2024-06-20 01:22:43 -0300

    [frontend] search input component implemented

---
+++ frontend/src/app/shared/components/search-input/search-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-input.component.html',
+  styles: ``,
+})
+export class SearchInputComponent {
+  public styles = input.required<string>();
+  public label = input.required<string>();
+
+  public onSearch = output<string>();
+
+  public searchQuery(searchTerm: string): void {
+    this.onSearch.emit(searchTerm);
+  }
+}
