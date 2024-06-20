72f497dcb0ff45900aa4fcd35ccac170678d1dbf
Martín Cruz <martincruz1426@gmail.com>
2024-06-20 16:17:29 -0300

    [backend] product sorts enum added

---
+++ backend/src/modules/products/interfaces/sort-by.enum.ts
@@ -0,0 +1,9 @@
+export enum SortBy {
+  NEWEST = 'newest',
+  OLDEST = 'oldest',
+  LAST_UPDATED = 'last_updated',
+  PRICE_DESC = 'price_desc',
+  PRICE_ASC = 'price_asc',
+  STOCK_DESC = 'stock_desc',
+  STOCK_ASC = 'stock_asc',
+}
