8eb1578b0bf7802b4255378f2d9b8fc9216704b0
Martín Cruz <martincruz1426@gmail.com>
2024-06-22 17:02:00 -0300

    [frontend] fixing and using filters service in products dashboard

--- frontend/src/app/pages/admin/pages/products-dashboard/products-dashboard.component.ts
+++ frontend/src/app/pages/admin/pages/products-dashboard/products-dashboard.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Product } from '../../../../core/interfaces/products';
 import {
-  Product,
-  ProductFilters,
-  SortBy,
-} from '../../../../core/interfaces/products';
-import { PaginationService, ProductsService } from '../../../../core/services';
+  FiltersService,
+  PaginationService,
+  ProductsService,
+} from '../../../../core/services';
 import { RouterModule } from '@angular/router';
 import { PaginationButtons } from '../../../../core/interfaces/pagination';
 import { SearchInputComponent } from '../../../../shared/components/search-input/search-input.component';
+import { SortBy } from '../../../../core/interfaces/filters';
 
 @Component({
   selector: 'admin-products-dashboard',
@@ -21,6 +22,7 @@ import { SearchInputComponent } from '../../../../shared/components/search-input
 export class ProductsDashboardComponent implements OnInit {
   private readonly productsService = inject(ProductsService);
   private readonly paginationService = inject(PaginationService);
+  private readonly filtersService = inject(FiltersService);
 
   public products = signal<Product[]>([]);
   public currentProduct = signal<Product | undefined>(undefined);
@@ -30,21 +32,22 @@ export class ProductsDashboardComponent implements OnInit {
   );
 
   public sortBy = SortBy;
-  public filter = signal<ProductFilters>({
-    search: '',
-    sortBy: SortBy.NEWEST,
-  });
+  public filter = computed(() => this.filtersService.filter());
 
   constructor() {}
 
   ngOnInit(): void {
     this.paginationService.setPagination(1, 5);
+    this.filtersService.resetFilters();
     this.getAllProducts();
   }
 
   public getAllProducts(): void {
     this.productsService
-      .getAllProducts(this.paginationService.pagination(), this.filter())
+      .getAllProducts(
+        this.paginationService.pagination(),
+        this.filtersService.filter()
+      )
       .subscribe({
         next: ({ next, prev, items }) => {
           this.products.set(items);
@@ -63,20 +66,18 @@ export class ProductsDashboardComponent implements OnInit {
       });
   }
 
+  public applyFilter(filter: SortBy): void {
+    this.filtersService.applyFilter(filter, { page: 1, limit: 5 });
+    this.getAllProducts();
+  }
+
+  public searchProduct(query: string): void {
+    this.filtersService.search(query, { page: 1, limit: 5 });
+    this.getAllProducts();
+  }
+
   public updatePage(value: number): void {
     this.paginationService.updatePage(value);
-    this.getAllProducts();
-  }
-
-  public applyFilter(sortBy: SortBy): void {
-    this.paginationService.setPagination(1, 5);
-    this.filter.update((currentFilter) => ({ ...currentFilter, sortBy }));
-    this.getAllProducts();
-  }
-
-  public searchProduct(search: string): void {
-    this.paginationService.setPagination(1, 5);
-    this.filter.update((currentFilter) => ({ ...currentFilter, search }));
     this.getAllProducts();
   }
 }
