ccac2a4a28fc7b129d02bceecdb7a80041e1fbed
Martín Cruz <martincruz1426@gmail.com>
2024-06-22 16:54:27 -0300

    [frontend] implementing filters service

---
+++ frontend/src/app/core/services/filters.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+
+import { Filters, SortBy } from '../interfaces/filters';
+import { PaginationService } from './pagination.service';
+import { Pagination } from '../interfaces/pagination';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FiltersService {
+  private readonly paginationService = inject(PaginationService);
+
+  public filter = signal<Filters>({
+    search: '',
+    sortBy: SortBy.NEWEST,
+  });
+
+  constructor() {}
+
+  public resetFilters(): void {
+    this.filter.set({ search: '', sortBy: SortBy.NEWEST });
+  }
+
+  public applyFilter(sortBy: SortBy, { page, limit }: Pagination): void {
+    this.paginationService.setPagination(page, limit);
+    this.filter.update((currentFilter) => ({ ...currentFilter, sortBy }));
+  }
+
+  public search(search: string, { page, limit }: Pagination): void {
+    this.paginationService.setPagination(page, limit);
+    this.filter.update((currentFilter) => ({ ...currentFilter, search }));
+  }
+
+  public getFiltersQuery(filters: Filters): string {
+    let filtersQuery = ``;
+
+    if (filters.search) filtersQuery += `&search=${filters.search}`;
+    if (filters.sortBy) filtersQuery += `&sortBy=${filters.sortBy}`;
+
+    return filtersQuery;
+  }
+}
