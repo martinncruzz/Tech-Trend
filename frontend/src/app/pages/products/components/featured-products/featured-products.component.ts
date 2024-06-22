c4da33df280706dbb3cf74f87daf764b08ac4c74
Martín Cruz <martincruz1426@gmail.com>
2024-06-22 17:53:31 -0300

    [frontend] showing last 4 products in featured products component

--- frontend/src/app/pages/products/components/featured-products/featured-products.component.ts
+++ frontend/src/app/pages/products/components/featured-products/featured-products.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-interface Product {
-  name: string;
-  description: string;
-  price: number;
-}
+import {
+  FiltersService,
+  PaginationService,
+  ProductsService,
+} from '../../../../core/services';
+import { Product } from '../../../../core/interfaces/products';
 
 @Component({
   selector: 'products-featured-products',
@@ -15,28 +15,29 @@ interface Product {
   templateUrl: './featured-products.component.html',
   styles: ``,
 })
-export class FeaturedProductsComponent {
-  public products = signal<Product[]>([
-    {
-      name: 'Gaming Laptop',
-      description: 'High-performance laptop for gaming',
-      price: 1499,
-    },
-    {
-      name: 'Mechanical Keyboard',
-      description:
-        'Tactile and responsive keyboard which you need in your setup',
-      price: 99,
-    },
-    {
-      name: 'Wireless Mouse',
-      description: 'Precise and comfortable mouse maded for you',
-      price: 49,
-    },
-    {
-      name: 'Gaming Headset',
-      description: 'Immersive audio experience for gaming and music production',
-      price: 79,
-    },
-  ]);
+export class FeaturedProductsComponent implements OnInit {
+  private readonly productsService = inject(ProductsService);
+  private readonly paginationService = inject(PaginationService);
+  private readonly filtersService = inject(FiltersService);
+
+  public products = signal<Product[]>([]);
+
+  ngOnInit(): void {
+    this.paginationService.setPagination(1, 4);
+    this.getAllProducts();
+  }
+
+  public getAllProducts(): void {
+    this.productsService
+      .getAllProducts(
+        this.paginationService.pagination(),
+        this.filtersService.filter()
+      )
+      .subscribe({
+        next: ({ items }) => {
+          this.products.set(items);
+        },
+        error: (err) => console.log(err),
+      });
+  }
 }
