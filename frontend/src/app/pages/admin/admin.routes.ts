afa753d91cd1801147b333c0f0ebc4e8a67c94be
Martín Cruz <martincruz1426@gmail.com>
2024-06-22 17:03:59 -0300

    [frontend] categories dashboard page added

--- frontend/src/app/pages/admin/admin.routes.ts
+++ frontend/src/app/pages/admin/admin.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProductsDashboardComponent } from './pages/products-dashboard/products-dashboard.component';
 import { ProductFormComponent } from './pages/product-form/product-form.component';
+import { CategoriesDashboardComponent } from './pages/categories-dashboard/categories-dashboard.component';
 
 export const ADMIN_ROUTES: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'products-dashboard', component: ProductsDashboardComponent },
   { path: 'product-form/new-product', component: ProductFormComponent },
   { path: 'product-form/edit/:id', component: ProductFormComponent },
+  { path: 'categories-dashboard', component: CategoriesDashboardComponent },
   { path: '**', redirectTo: 'dashboard' },
 ];
---
+++ frontend/src/app/pages/admin/pages/categories-dashboard/categories-dashboard.component.ts
@@ -0,0 +1,79 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SearchInputComponent } from '../../../../shared/components/search-input/search-input.component';
+import {
+  CategoriesService,
+  FiltersService,
+  PaginationService,
+} from '../../../../core/services';
+import { PaginationButtons } from '../../../../core/interfaces/pagination';
+import { Category } from '../../../../core/interfaces/categories';
+import { SortBy } from '../../../../core/interfaces/filters';
+
+@Component({
+  selector: 'admin-categories-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterModule, SearchInputComponent],
+  templateUrl: './categories-dashboard.component.html',
+  styles: ``,
+})
+export class CategoriesDashboardComponent implements OnInit {
+  private readonly categoriesService = inject(CategoriesService);
+  private readonly paginationService = inject(PaginationService);
+  private readonly filtersService = inject(FiltersService);
+
+  public categories = signal<Category[]>([]);
+  public currentCategory = signal<Category | undefined>(undefined);
+
+  public paginationButtons = computed<PaginationButtons>(() =>
+    this.paginationService.paginationButtons()
+  );
+
+  public sortBy = SortBy;
+  public filter = computed(() => this.filtersService.filter());
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.paginationService.setPagination(1, 5);
+    this.filtersService.resetFilters();
+    this.getAllCategories();
+  }
+
+  public getAllCategories(): void {
+    this.categoriesService
+      .getAllCategories(this.paginationService.pagination(), this.filter())
+      .subscribe({
+        next: ({ next, prev, items }) => {
+          this.categories.set(items);
+          this.paginationService.setPaginationButtons(!!next, !!prev);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  public deleteProduct(): void {
+    this.categoriesService
+      .deleteCategory(this.currentCategory()!.category_id)
+      .subscribe({
+        next: () => this.getAllCategories(),
+        error: (err) => console.log(err),
+      });
+  }
+
+  public applyFilter(filter: SortBy): void {
+    this.filtersService.applyFilter(filter, { page: 1, limit: 5 });
+    this.getAllCategories();
+  }
+
+  public searchCategory(query: string): void {
+    this.filtersService.search(query, { page: 1, limit: 5 });
+    this.getAllCategories();
+  }
+
+  public updatePage(value: number): void {
+    this.paginationService.updatePage(value);
+    this.getAllCategories();
+  }
+}
