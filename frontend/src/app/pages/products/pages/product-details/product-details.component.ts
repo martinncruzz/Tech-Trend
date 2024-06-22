2373c44a28edfa5026c4c1d6ef27f219b4e9690a
Martín Cruz <martincruz1426@gmail.com>
2024-06-22 17:41:35 -0300

    [frontend] showing product real data in product details page

--- frontend/src/app/pages/products/pages/product-details/product-details.component.ts
+++ frontend/src/app/pages/products/pages/product-details/product-details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ProductsService } from '../../../../core/services';
+import { Product } from '../../../../core/interfaces/products';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
-  selector: 'app-product-details',
+  selector: 'products-product-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details.component.html',
   styles: ``,
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent implements OnInit {
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  private readonly productsService = inject(ProductsService);
+
+  public currentProduct = signal<Product | undefined>(undefined);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.productsService.getProductById(id)))
+      .subscribe({
+        next: (product) => {
+          if (product.stock === 0) this.router.navigateByUrl('/products');
+          this.currentProduct.set(product);
+        },
+        error: (error) => this.router.navigateByUrl('/products'),
+      });
+  }
+}
