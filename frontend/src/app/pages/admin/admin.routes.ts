81be7511432d4cc62da690e24ccc71e04f127d70
Martín Cruz <martincruz1426@gmail.com>
2024-06-24 15:50:42 -0300

    [frontend] users dashboard page added

--- frontend/src/app/pages/admin/admin.routes.ts
+++ frontend/src/app/pages/admin/admin.routes.ts
@@ -5,6 +5,7 @@ import { ProductsDashboardComponent } from './pages/products-dashboard/products-
 import { ProductFormComponent } from './pages/product-form/product-form.component';
 import { CategoriesDashboardComponent } from './pages/categories-dashboard/categories-dashboard.component';
 import { CategoryFormComponent } from './pages/category-form/category-form.component';
+import { UsersDashboardComponent } from './pages/users-dashboard/users-dashboard.component';
 
 export const ADMIN_ROUTES: Routes = [
   { path: 'dashboard', component: DashboardComponent },
@@ -14,5 +15,6 @@ export const ADMIN_ROUTES: Routes = [
   { path: 'categories-dashboard', component: CategoriesDashboardComponent },
   { path: 'category-form/new-category', component: CategoryFormComponent },
   { path: 'category-form/edit/:id', component: CategoryFormComponent },
+  { path: 'users-dashboard', component: UsersDashboardComponent },
   { path: '**', redirectTo: 'dashboard' },
 ];
---
+++ frontend/src/app/pages/admin/pages/users-dashboard/users-dashboard.component.ts
@@ -0,0 +1,150 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SearchInputComponent } from '../../../../shared/components/search-input/search-input.component';
+import {
+  FiltersService,
+  PaginationService,
+  UsersService,
+  ValidatorsService,
+} from '../../../../core/services';
+import { SortBy } from '../../../../core/interfaces/filters';
+import { PaginationButtons } from '../../../../core/interfaces/pagination';
+import { User } from '../../../../core/interfaces/users';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { patterns } from '../../../../core/constants';
+
+@Component({
+  selector: 'admin-users-dashboard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SearchInputComponent,
+  ],
+  templateUrl: './users-dashboard.component.html',
+  styles: ``,
+})
+export class UsersDashboardComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+
+  private readonly usersService = inject(UsersService);
+  private readonly paginationService = inject(PaginationService);
+  private readonly filtersService = inject(FiltersService);
+  private readonly validatorsService = inject(ValidatorsService);
+
+  public users = signal<User[]>([]);
+  public currentUser = signal<User | undefined>(undefined);
+
+  public paginationButtons = computed<PaginationButtons>(() =>
+    this.paginationService.paginationButtons()
+  );
+
+  public sortBy = SortBy;
+  public filter = computed(() => this.filtersService.filter());
+
+  public errorMessage = signal<string | null>(null);
+
+  public updateUserForm: FormGroup = this.fb.group({
+    fullname: ['', [Validators.required, Validators.minLength(2)]],
+    address: ['', [Validators.required, Validators.minLength(5)]],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.pattern(patterns.email),
+      ],
+    ],
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.paginationService.setPagination(1, 5);
+    this.filtersService.resetFilters();
+    this.getAllUsers();
+  }
+
+  public getAllUsers(): void {
+    this.usersService
+      .getAllUsers(
+        this.paginationService.pagination(),
+        this.filtersService.filter()
+      )
+      .subscribe({
+        next: ({ next, prev, items }) => {
+          this.users.set(items);
+          this.paginationService.setPaginationButtons(!!next, !!prev);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  public getUserById(user: User): void {
+    this.currentUser.set(user);
+
+    this.usersService.getUserById(this.currentUser()!.user_id).subscribe({
+      next: (user) => this.updateUserForm.reset(user),
+      error: (error) => console.log(error),
+    });
+  }
+
+  public updateUser(): void {
+    this.usersService
+      .updateUser(this.updateUserForm.value, this.currentUser()!.user_id)
+      .subscribe({
+        next: () => this.getAllUsers(),
+        error: (error) => this.errorMessage.set(error),
+      });
+  }
+
+  public deleteUser(): void {
+    this.usersService.deleteUser(this.currentUser()!.user_id).subscribe({
+      next: () => this.getAllUsers(),
+      error: (error) => console.log(error),
+    });
+  }
+
+  public applyFilter(filter: SortBy): void {
+    this.filtersService.applyFilter(filter, { page: 1, limit: 5 });
+    this.getAllUsers();
+  }
+
+  public searchCategory(query: string): void {
+    this.filtersService.search(query, { page: 1, limit: 5 });
+    this.getAllUsers();
+  }
+
+  public updatePage(value: number): void {
+    this.paginationService.updatePage(value);
+    this.getAllUsers();
+  }
+
+  public isInvalidField(field: string): boolean | null {
+    return this.validatorsService.isInvalidField(this.updateUserForm, field);
+  }
+
+  public getFieldError(field: string): string | null {
+    return this.validatorsService.getFieldError(this.updateUserForm, field);
+  }
+
+  public resetForm(): void {
+    this.updateUserForm.reset();
+    this.resetFormErrors();
+  }
+
+  private resetFormErrors(): void {
+    this.updateUserForm.valueChanges.subscribe(() => {
+      this.errorMessage.set(null);
+    });
+  }
+}
