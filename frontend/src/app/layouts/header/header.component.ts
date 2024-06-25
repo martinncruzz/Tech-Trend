e5ded6e03c95c908b44530ae14598a5355905969
Martín Cruz <martincruz1426@gmail.com>
2024-06-25 16:49:53 -0300

    [frontend] get all categories in header and show it in the navbar

--- frontend/src/app/layouts/header/header.component.ts
+++ frontend/src/app/layouts/header/header.component.ts
@@ -1,8 +1,14 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { AuthService } from '../../core/services';
+import {
+  AuthService,
+  CategoriesService,
+  FiltersService,
+  PaginationService,
+} from '../../core/services';
+import { Category } from '../../core/interfaces/categories';
 
 @Component({
   selector: 'layouts-header',
@@ -11,12 +17,34 @@ import { AuthService } from '../../core/services';
   templateUrl: './header.component.html',
   styles: ``,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   private readonly authService = inject(AuthService);
+  private readonly categoriesService = inject(CategoriesService);
+  private readonly paginationService = inject(PaginationService);
+  private readonly filtersService = inject(FiltersService);
 
   public isLoggedIn = computed(() => this.authService.isLoggedIn());
+  public categories = signal<Category[]>([]);
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  public getAllCategories(): void {
+    this.categoriesService
+      .getAllCategories(
+        this.paginationService.pagination(),
+        this.filtersService.filter()
+      )
+      .subscribe({
+        next: ({ items }) => {
+          this.categories.set(items);
+        },
+        error: (error) => console.log(error),
+      });
+  }
 
   public logout(): void {
     this.authService.logout();
