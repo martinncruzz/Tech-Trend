bf204466654279a1527f8851831a3e0be35f2ce0
Martín Cruz <martincruz1426@gmail.com>
2024-06-25 16:51:03 -0300

    [frontend] filters and search implemented in products list

--- frontend/src/app/pages/products/pages/products-list/products-list.component.ts
+++ frontend/src/app/pages/products/pages/products-list/products-list.component.ts
@@ -4,33 +4,62 @@ import { RouterModule } from '@angular/router';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
 import { Product } from '../../../../core/interfaces/products';
 import {
+  CategoriesService,
   FiltersService,
   PaginationService,
   ProductsService,
 } from '../../../../core/services';
+import { Category } from '../../../../core/interfaces/categories';
+import { SearchInputComponent } from '../../../../shared/components/search-input/search-input.component';
+import { SortBy } from '../../../../core/interfaces/filters';
 
 @Component({
   selector: 'products-products-list',
   standalone: true,
-  imports: [RouterModule, ProductCardComponent],
+  imports: [RouterModule, ProductCardComponent, SearchInputComponent],
   templateUrl: './products-list.component.html',
   styles: ``,
 })
 export class ProductsListComponent implements OnInit {
   private readonly productsService = inject(ProductsService);
+  private readonly categoriesService = inject(CategoriesService);
   private readonly paginationService = inject(PaginationService);
   private readonly filtersService = inject(FiltersService);
 
+  public filters = signal([
+    { label: 'Newest', sort: SortBy.NEWEST },
+    { label: 'Oldest', sort: SortBy.OLDEST },
+    { label: 'From lowest to highest price', sort: SortBy.PRICE_ASC },
+    { label: 'From highest to lowest price', sort: SortBy.PRICE_DESC },
+    { label: 'From lowest to highest stock', sort: SortBy.STOCK_ASC },
+    { label: 'From highest to lowest stock', sort: SortBy.STOCK_DESC },
+  ]);
   public products = signal<Product[]>([]);
+  public categories = signal<Category[]>([]);
 
   public paginationButtons = computed(() =>
     this.paginationService.paginationButtons()
   );
 
   ngOnInit(): void {
-    this.paginationService.setPagination(1, 9);
     this.filtersService.resetFilters();
+    this.paginationService.setPagination(1, 9);
+    this.getAllCategories();
     this.getAllProducts();
+  }
+
+  public getAllCategories(): void {
+    this.categoriesService
+      .getAllCategories(
+        this.paginationService.pagination(),
+        this.filtersService.filter()
+      )
+      .subscribe({
+        next: ({ items }) => {
+          this.categories.set(items);
+        },
+        error: (error) => console.log(error),
+      });
   }
 
   public getAllProducts(): void {
@@ -47,6 +76,18 @@ export class ProductsListComponent implements OnInit {
         },
         error: (err) => console.log(err),
       });
+  }
+
+  public searchProduct(query: string): void {
+    this.filtersService.search(query, { page: 1, limit: 9 });
+    this.getAllProducts();
+  }
+
+  public applyFilter(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const filter = selectElement.value as SortBy;
+    this.filtersService.applyFilter(filter, { page: 1, limit: 9 });
+    this.getAllProducts();
   }
 
   public updatePage(value: number): void {
