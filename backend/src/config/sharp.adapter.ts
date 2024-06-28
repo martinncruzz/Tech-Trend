79110ef56300318211266d450df8f67ff2651be1
Martín Cruz <martincruz1426@gmail.com>
2024-06-28 15:42:29 -0300

    [backend] sharp adapter updated

--- backend/src/config/sharp.adapter.ts
+++ backend/src/config/sharp.adapter.ts
@@ -2,5 +2,5 @@ import * as sharp from 'sharp';
 
 export class SharpAdapter {
   static optimizeImage = async (file: Express.Multer.File) =>
-    await sharp(file.buffer).resize({ width: 1920 }).webp().toBuffer();
+    await sharp(file.buffer).webp().toBuffer();
 }
