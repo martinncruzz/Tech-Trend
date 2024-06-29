b00ff23815e2f050e14db9e88274eeb9ab5c4e4b
Martín Cruz <martincruz1426@gmail.com>
2024-06-29 00:46:29 -0300

    [frontend] udpating category interface

--- frontend/src/app/core/interfaces/categories/category.interface.ts
+++ frontend/src/app/core/interfaces/categories/category.interface.ts
@@ -1,7 +1,10 @@
+import { Product } from '../products';
+
 export interface Category {
   category_id: string;
   name: string;
   description: string;
   createdAt: Date;
   updatedAt: Date | null;
+  products: Product[];
 }
