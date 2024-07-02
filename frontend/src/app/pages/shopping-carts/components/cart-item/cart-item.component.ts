76e5e4271c4273e1ecf90b4f287f46cefb5b9d49
Martín Cruz <martincruz1426@gmail.com>
2024-07-02 18:39:43 -0300

    [frontend] cart item component added

---
+++ frontend/src/app/pages/shopping-carts/components/cart-item/cart-item.component.ts
@@ -0,0 +1,37 @@
+import { Component, input, output } from '@angular/core';
+import {
+  ProductItem,
+  ShoppingCartForm,
+} from '../../../../core/interfaces/shopping-carts';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'shopping-carts-cart-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cart-item.component.html',
+  styles: ``,
+})
+export class CartItemComponent {
+  public productItem = input.required<ProductItem>();
+
+  public newQuantity = output<ShoppingCartForm>();
+
+  public productToRemoveFromCart = output<string>();
+  public productRemovalConfirmation = output<boolean>();
+
+  public updateProductQuantity(quantity: number): void {
+    this.newQuantity.emit({
+      product_id: this.productItem().product_id,
+      quantity,
+    });
+  }
+
+  public setProductToRemoveFromCart(productId: string): void {
+    this.productToRemoveFromCart.emit(productId);
+  }
+
+  public confirmRemoval(confirm: boolean): void {
+    this.productRemovalConfirmation.emit(confirm);
+  }
+}
