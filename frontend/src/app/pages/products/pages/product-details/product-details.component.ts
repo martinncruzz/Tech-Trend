073d1042117b991ab70e4aef72fbeb3283a143a0
Martín Cruz <martincruz1426@gmail.com>
2024-07-02 18:38:03 -0300

    [frontend] implementing add product to cart, updating validations and styles in product details page

--- frontend/src/app/pages/products/pages/product-details/product-details.component.ts
+++ frontend/src/app/pages/products/pages/product-details/product-details.component.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { ProductsService, ValidatorsService } from '../../../../core/services';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import {
+  ProductsService,
+  ShoppingCartsService,
+  ValidatorsService,
+} from '../../../../core/services';
 import { Product } from '../../../../core/interfaces/products';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { GetShoppingCartResponse } from '../../../../core/interfaces/shopping-carts';
 
 @Component({
   selector: 'products-product-details',
@@ -19,13 +30,20 @@ export class ProductDetailsComponent implements OnInit {
   private readonly activatedRoute = inject(ActivatedRoute);
 
   private readonly productsService = inject(ProductsService);
+  private readonly shoppingCartsService = inject(ShoppingCartsService);
   private readonly validatorsService = inject(ValidatorsService);
 
   public errorMessage = signal<string | null>(null);
   public currentProduct = signal<Product | undefined>(undefined);
+  public productId = signal<string>('');
+
+  public shoppingCart = signal<GetShoppingCartResponse | undefined>(undefined);
+  public productIdsInCart = computed(() =>
+    this.shoppingCartsService.productIdsInCart()
+  );
 
   public quantityForm: FormGroup = this.fb.group({
-    quantity: [1, [Validators.required, Validators.min(1)]],
+    quantity: [1],
   });
 
   constructor() {}
@@ -37,8 +55,40 @@ export class ProductDetailsComponent implements OnInit {
         next: (product) => {
           if (product.stock === 0) this.router.navigateByUrl('/products');
           this.currentProduct.set(product);
+          this.productId.set(product.product_id);
+          this.setMaxQuantityValidation(product);
         },
         error: (error) => this.router.navigateByUrl('/products'),
+      });
+
+    this.getUserShoppingCart();
+  }
+
+  public getUserShoppingCart(): void {
+    this.shoppingCartsService.getUserShoppingCart().subscribe({
+      next: (shoppingCart) => this.shoppingCart.set(shoppingCart),
+      error: (error) => console.log(error),
+    });
+  }
+
+  public addProductToCart(): void {
+    this.shoppingCartsService
+      .addProductToCart({
+        product_id: this.currentProduct()!.product_id,
+        quantity: this.quantityForm.controls['quantity'].value,
+      })
+      .subscribe({
+        next: () => this.getUserShoppingCart(),
+        error: (error) => this.errorMessage.set(error),
+      });
+  }
+
+  public removeProductFromCart(): void {
+    this.shoppingCartsService
+      .removeProductFromCart(this.productId())
+      .subscribe({
+        next: () => this.getUserShoppingCart(),
+        error: (error) => this.errorMessage.set(error),
       });
   }
 
@@ -60,5 +110,14 @@ export class ProductDetailsComponent implements OnInit {
     this.quantityForm.valueChanges.subscribe(() => {
       this.errorMessage.set(null);
     });
+  }
+
+  private setMaxQuantityValidation(product: Product): void {
+    this.quantityForm.controls['quantity'].setValidators([
+      Validators.required,
+      Validators.min(1),
+      Validators.max(product.stock),
+    ]);
+    this.quantityForm.controls['quantity'].updateValueAndValidity();
   }
 }
