aa634fd99b874d17e984f84a7c4a9ef2ae8a173f
Martín Cruz <martincruz1426@gmail.com>
2024-07-02 18:39:57 -0300

    [frontend] shopping cart page implemented

--- frontend/src/app/pages/shopping-carts/shopping-carts.component.ts
+++ frontend/src/app/pages/shopping-carts/shopping-carts.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import {
+  GetShoppingCartResponse,
+  ShoppingCartForm,
+} from '../../core/interfaces/shopping-carts';
+import { ShoppingCartsService } from '../../core/services';
+import { CommonModule } from '@angular/common';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shopping-carts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CartItemComponent],
   templateUrl: './shopping-carts.component.html',
   styles: ``,
 })
-export class ShoppingCartsComponent {}
+export class ShoppingCartsComponent implements OnInit {
+  private readonly router = inject(Router);
+
+  private readonly shoppingCartsService = inject(ShoppingCartsService);
+
+  public shoppingCart = signal<GetShoppingCartResponse | undefined>(undefined);
+  public productToRemoveFromCart = signal<string>('');
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getUserShoppingCart();
+  }
+
+  public getUserShoppingCart(): void {
+    this.shoppingCartsService.getUserShoppingCart().subscribe({
+      next: (shoppingCart) => this.shoppingCart.set(shoppingCart),
+      error: () => this.router.navigateByUrl('/'),
+    });
+  }
+
+  public updateProductQuantity(shoppingCartForm: ShoppingCartForm): void {
+    this.shoppingCartsService
+      .updateProductQuantityInCart(shoppingCartForm)
+      .subscribe({
+        next: () => this.getUserShoppingCart(),
+        error: (error) => console.log(error),
+      });
+  }
+
+  public getProductToRemoveFromCart(productId: string): void {
+    this.productToRemoveFromCart.set(productId);
+  }
+
+  public removeProductFromCart(confirm: boolean): void {
+    if (confirm) {
+      this.shoppingCartsService
+        .removeProductFromCart(this.productToRemoveFromCart())
+        .subscribe({
+          next: () => this.getUserShoppingCart(),
+          error: (error) => console.log(error),
+        });
+    }
+  }
+}
