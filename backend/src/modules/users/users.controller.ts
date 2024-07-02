e06bcc3df2509ffb9a70266b336549354f766f58
Martín Cruz <martincruz1426@gmail.com>
2024-07-02 18:52:54 -0300

    [backend] routes protection in users module

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -12,22 +12,27 @@ import {
 import { Filters } from '../shared/dtos';
 import { UpdateUserDto } from './dtos';
 import { UsersService } from './users.service';
+import { Auth } from '../auth/decorators';
+import { ValidRoles } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Auth(ValidRoles.admin)
   getAllUsers(@Query() params: Filters) {
     return this.usersService.getAllUsers(params);
   }
 
   @Get(':id')
+  @Auth(ValidRoles.admin)
   getUserById(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.getUserById(id);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   updateUser(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -36,6 +41,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   deleteUser(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.deleteUser(id);
   }
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/database/prisma.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
