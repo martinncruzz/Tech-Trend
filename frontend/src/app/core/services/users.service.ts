35cc5bb8ae0263a0247782e7fe74160a3fd41038
Martín Cruz <martincruz1426@gmail.com>
2024-07-03 16:11:11 -0300

    [frontend] include token in headers, users service

--- frontend/src/app/core/services/users.service.ts
+++ frontend/src/app/core/services/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { FiltersService } from './filters.service';
@@ -24,28 +24,41 @@ export class UsersService {
   ): Observable<GetAllUsersResponse> {
     const filtersQuery = this.filtersService.getFiltersQuery(filters);
 
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
     return this.http
       .get<GetAllUsersResponse>(
-        `${this.backendUrl}/users?page=${page}&limit=${limit}${filtersQuery}`
+        `${this.backendUrl}/users?page=${page}&limit=${limit}${filtersQuery}`,
+        { headers }
       )
       .pipe(catchError((err) => throwError(() => err.error.message)));
   }
 
   public getUserById(id: string): Observable<User> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
     return this.http
-      .get<User>(`${this.backendUrl}/users/${id}`)
+      .get<User>(`${this.backendUrl}/users/${id}`, { headers })
       .pipe(catchError((err) => throwError(() => err.error.message)));
   }
 
   public updateUser(userForm: UserForm, id: string): Observable<User> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
     return this.http
-      .patch<User>(`${this.backendUrl}/users/${id}`, userForm)
+      .patch<User>(`${this.backendUrl}/users/${id}`, userForm, { headers })
       .pipe(catchError((err) => throwError(() => err.error.message)));
   }
 
   public deleteUser(id: string): Observable<boolean> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
     return this.http
-      .delete<boolean>(`${this.backendUrl}/users/${id}`)
+      .delete<boolean>(`${this.backendUrl}/users/${id}`, { headers })
       .pipe(catchError((err) => throwError(() => err.error.message)));
   }
 }
