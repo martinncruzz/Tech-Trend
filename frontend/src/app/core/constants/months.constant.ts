10cea0f3e7a3fcb499d85adcf857904fc440cece
Martín Cruz <martincruz1426@gmail.com>
2024-07-14 00:35:19 -0300

    [frontend] creating constant of months and its type

---
+++ frontend/src/app/core/constants/months.constant.ts
@@ -0,0 +1,28 @@
+type Months =
+  | 'January'
+  | 'Febraury'
+  | 'March'
+  | 'April'
+  | 'May'
+  | 'June'
+  | 'July'
+  | 'August'
+  | 'September'
+  | 'October'
+  | 'November'
+  | 'December';
+
+export const MONTHS: Months[] = [
+  'January',
+  'Febraury',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
