79de7ec68b955dbff7195753ecc4a4eb8757a93c
Martín Cruz <martincruz1426@gmail.com>
2024-11-29 11:07:40 -0300

    [api] organize module exports and update imports in AppModule and main.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,23 +1,26 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './database/prisma.module';
-import { SharedModule } from './modules/shared/shared.module';
-import { UsersModule } from './modules/users/users.module';
-import { ProductsModule } from './modules/products/products.module';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { ShoppingCartsModule } from './modules/shopping-carts/shopping-carts.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { OrdersModule } from './modules/orders/orders.module';
-import { PaymentsModule } from './modules/payments/payments.module';
+
+import { PrismaModule } from './database';
+import {
+  SharedModule,
+  UsersModule,
+  AuthModule,
+  ProductsModule,
+  CategoriesModule,
+  ShoppingCartsModule,
+  OrdersModule,
+  PaymentsModule,
+} from './modules';
 
 @Module({
   imports: [
     PrismaModule,
     SharedModule,
     UsersModule,
+    AuthModule,
     ProductsModule,
     CategoriesModule,
     ShoppingCartsModule,
-    AuthModule,
     OrdersModule,
     PaymentsModule,
   ],
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,25 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { envs } from './config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+
+import { envs } from './config';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-  });
+  const app = await NestFactory.create(AppModule, { rawBody: true });
 
   app.enableCors({ origin: envs.FRONTEND_URL });
-
   app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
   await app.listen(envs.PORT);
   logger.log(`Server running on port ${envs.PORT}`);
