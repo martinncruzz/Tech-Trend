9892320a043993d0f6bfd615da37d74df1533efb
Martín Cruz <martincruz1426@gmail.com>
2024-12-03 16:54:55 -0300

    [api] specify NestExpressApplication in app initialization

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { envs } from './config';
 import { CustomExceptionFilter } from './modules';
@@ -8,7 +9,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule, { rawBody: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { rawBody: true });
 
   app.enableCors({ origin: envs.FRONTEND_URL });
   app.setGlobalPrefix('api');
