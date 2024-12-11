f764aaf8d38275a646183590c21bc7eb9a2d2cdd
Martín Cruz <martincruz1426@gmail.com>
2024-12-11 15:30:23 -0300

    [api] fix: explicitly set default strategy in AuthGuard for Auth decorator

--- api/src/modules/auth/decorators/auth.decorator.ts
+++ api/src/modules/auth/decorators/auth.decorator.ts
@@ -1,9 +1,9 @@
-import { AuthGuard } from '@nestjs/passport';
 import { UseGuards, applyDecorators } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { ValidRoles } from '@prisma/client';
 
 import { RoleProtected, UserRoleGuard } from '..';
 
 export function Auth(...roles: ValidRoles[]) {
-  return applyDecorators(RoleProtected(...roles), UseGuards(AuthGuard(), UserRoleGuard));
+  return applyDecorators(RoleProtected(...roles), UseGuards(AuthGuard('jwt'), UserRoleGuard));
 }
