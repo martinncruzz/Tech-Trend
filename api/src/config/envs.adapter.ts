352b8ab44252a3553f8cdb5898ecb17728be1764
Martín Cruz <martincruz1426@gmail.com>
2024-12-14 01:28:15 -0300

    [api] refactor: improve environment variable validation with zod

--- api/src/config/envs.adapter.ts
+++ api/src/config/envs.adapter.ts
@@ -3,16 +3,18 @@ import { z } from 'zod';
 process.loadEnvFile();
 
 const envsSchema = z.object({
-  PORT: z.coerce.number(),
-  FRONTEND_URL: z.string().url('FRONTEND_URL is required'),
-  BACKEND_URL: z.string().url('BACKEND_URL is required'),
-  DATABASE_URL: z.string().url('DATABASE_URL is required'),
-  JWT_SECRET: z.string().min(1, 'JWT_SECRET is required'),
-  CLOUDINARY_NAME: z.string().min(1, 'CLOUDINARY_NAME is required'),
-  CLOUDINARY_API_KEY: z.string().min(1, 'CLOUDINARY_API_KEY is required'),
-  CLOUDINARY_API_SECRET: z.string().min(1, 'CLOUDINARY_API_SECRET is required'),
-  STRIPE_SECRET_KEY: z.string().min(1, 'STRIPE_SECRET_KEY is required'),
-  STRIPE_ENDPOINT_SECRET: z.string().min(1, 'STRIPE_ENDPOINT_SECRET is required'),
+  PORT: z.coerce.number().int().positive('PORT must be a positive integer'),
+  FRONTEND_URL: z.string().url('FRONTEND_URL must be a valid URL'),
+  BACKEND_URL: z.string().url('BACKEND_URL must be a valid URL'),
+  DATABASE_URL: z.string().url('DATABASE_URL must be a valid URL'),
+  JWT_SECRET: z.string().nonempty('JWT_SECRET is required'),
+
+  CLOUDINARY_NAME: z.string().nonempty('CLOUDINARY_NAME is required'),
+  CLOUDINARY_API_KEY: z.string().nonempty('CLOUDINARY_API_KEY is required'),
+  CLOUDINARY_API_SECRET: z.string().nonempty('CLOUDINARY_API_SECRET is required'),
+
+  STRIPE_SECRET_KEY: z.string().nonempty('STRIPE_SECRET_KEY is required'),
+  STRIPE_ENDPOINT_SECRET: z.string().nonempty('STRIPE_ENDPOINT_SECRET is required'),
 });
 
 const { success, error, data } = envsSchema.safeParse(process.env);
@@ -22,17 +24,4 @@ if (!success) {
   process.exit(1);
 }
 
-export const envs = {
-  PORT: data.PORT,
-  FRONTEND_URL: data.FRONTEND_URL,
-  BACKEND_URL: data.BACKEND_URL,
-  DATABASE_URL: data.DATABASE_URL,
-  JWT_SECRET: data.JWT_SECRET,
-
-  CLOUDINARY_NAME: data.CLOUDINARY_NAME,
-  CLOUDINARY_API_KEY: data.CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET: data.CLOUDINARY_API_SECRET,
-
-  STRIPE_SECRET_KEY: data.STRIPE_SECRET_KEY,
-  STRIPE_ENDPOINT_SECRET: data.STRIPE_ENDPOINT_SECRET,
-};
+export const envs = data;
