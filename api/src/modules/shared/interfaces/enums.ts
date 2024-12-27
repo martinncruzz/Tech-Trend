6817fbec3c91aa39879e831174a16ece083d8009
Martín Cruz <martincruz1426@gmail.com>
2024-12-27 19:55:46 -0300

    [api] refactor: update enums to rename ResourceType and introduce UserRoles

--- api/src/modules/shared/interfaces/enums.ts
+++ api/src/modules/shared/interfaces/enums.ts
@@ -1,16 +1,11 @@
-export enum ResourceType {
-  categories = 'categories',
-  orders = 'orders',
-  products = 'products',
-  users = 'users',
+export enum UserRoles {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
 }
 
-export enum SortBy {
-  NEWEST = 'newest',
-  OLDEST = 'oldest',
-  LAST_UPDATED = 'last_updated',
-  PRICE_DESC = 'price_desc',
-  PRICE_ASC = 'price_asc',
-  STOCK_DESC = 'stock_desc',
-  STOCK_ASC = 'stock_asc',
+export enum ResourceType {
+  CATEGORIES = 'categories',
+  ORDERS = 'orders',
+  PRODUCTS = 'products',
+  USERS = 'users',
 }
