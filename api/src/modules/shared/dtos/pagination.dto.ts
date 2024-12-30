93c1f468c090a5a087fbeff66fa0d2bbd7c8ba9a
Martín Cruz <martincruz1426@gmail.com>
2024-12-30 20:09:40 -0300

    [api] refactor: update pagination DTO validation to use IsInt and IsPositive

--- api/src/modules/shared/dtos/pagination.dto.ts
+++ api/src/modules/shared/dtos/pagination.dto.ts
@@ -1,16 +1,16 @@
 import { Type } from 'class-transformer';
-import { IsNumber, IsOptional, Min } from 'class-validator';
+import { IsInt, IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
   @IsOptional()
-  @IsNumber()
-  @Min(1)
+  @IsInt()
+  @IsPositive()
   @Type(() => Number)
   page: number = 1;
 
   @IsOptional()
-  @IsNumber()
-  @Min(1)
+  @IsInt()
+  @IsPositive()
   @Type(() => Number)
   limit: number = 10;
 }
