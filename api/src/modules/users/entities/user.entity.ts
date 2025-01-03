05fd171be1476f23148e3eae15311beb211fa0d0
Martín Cruz <martincruz1426@gmail.com>
2025-01-03 19:02:01 -0300

    [api] feat: enhance User entity with constructor and fromObject method for improved data handling

--- api/src/modules/users/entities/user.entity.ts
+++ api/src/modules/users/entities/user.entity.ts
@@ -1,12 +1,38 @@
-import { ValidRoles } from '@prisma/client';
+import { InternalServerErrorException } from '@nestjs/common';
+
+import { Cart } from '@modules/carts/entities/cart.entity';
+import { Order } from '@modules/orders/entities/order.entity';
+import { UserRoles } from '@modules/shared/interfaces/enums';
+import { UserSchema } from '@config/schemas/user.schema';
+import { ValidatorAdapter } from '@config/adapters/validator.adapter';
 
 export class User {
-  user_id: string;
-  fullname: string;
-  address: string;
-  email: string;
-  password: string;
-  roles: ValidRoles[];
-  createdAt: Date;
-  updatedAt: Date | null;
+  constructor(
+    public readonly id: string,
+    public readonly fullname: string,
+    public readonly email: string,
+    public readonly password: string,
+    public readonly roles: UserRoles[],
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date,
+    public readonly cart?: Partial<Cart>,
+    public readonly orders?: Partial<Order>[],
+  ) {}
+
+  static fromObject(object: Record<string, any>): User {
+    const { success, data } = ValidatorAdapter.validate(object, UserSchema);
+
+    if (!success) throw new InternalServerErrorException('Error processing user data');
+
+    return new User(
+      data.id,
+      data.fullname,
+      data.email,
+      data.password,
+      data.roles,
+      data.createdAt,
+      data.updatedAt,
+      data.cart,
+    );
+  }
 }
