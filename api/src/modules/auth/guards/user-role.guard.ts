1851783def987f1f7b4c2c8036804f6f661bd66e
Martín Cruz <martincruz1426@gmail.com>
2025-01-04 18:30:36 -0300

    [api] feat: update UserRoleGuard to use UserRoles enum for role validation

--- api/src/modules/auth/guards/user-role.guard.ts
+++ api/src/modules/auth/guards/user-role.guard.ts
@@ -1,16 +1,16 @@
 import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ValidRoles } from '@prisma/client';
 
-import { User } from '../../users';
-import { META_ROLES } from '..';
+import { METADATA_ROLES_KEY } from '@modules/auth/decorators/role-protected.decorator';
+import { User } from '@modules/users/entities/user.entity';
+import { UserRoles } from '@modules/shared/interfaces/enums';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const validRoles: ValidRoles[] = this.reflector.get(META_ROLES, context.getHandler());
+    const validRoles: UserRoles[] = this.reflector.get(METADATA_ROLES_KEY, context.getHandler());
 
     if (!validRoles) return true;
     if (validRoles.length === 0) return true;
