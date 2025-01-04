5436deaebf46941169720c1bfe74cac94a70cc9b
Martín Cruz <martincruz1426@gmail.com>
2025-01-04 18:31:09 -0300

    [api] feat: refactor JwtStrategy to use UsersService for user retrieval and update JwtPayload to use 'id' instead of 'user_id'

--- api/src/modules/auth/strategies/jwt.strategy.ts
+++ api/src/modules/auth/strategies/jwt.strategy.ts
@@ -2,24 +2,21 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-import { envs } from '../../../config';
-import { PrismaService } from '../../../database';
-import { User } from '../../users';
-import { JwtPayload } from '..';
+import { envs } from '@config/adapters/envs.adapter';
+import { JwtPayload } from '@modules/auth/interfaces/jwt-payload.interface';
+import { User } from '@modules/users/entities/user.entity';
+import { UsersService } from '@modules/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly prismaService: PrismaService) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: envs.JWT_SECRET,
-    });
+  constructor(private readonly usersService: UsersService) {
+    super({ jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), secretOrKey: envs.JWT_SECRET });
   }
 
   async validate(payload: JwtPayload): Promise<User> {
-    const { user_id } = payload;
+    const { id } = payload;
 
-    const user = await this.prismaService.user.findUnique({ where: { user_id } });
+    const user = await this.usersService.getUserById(id);
     if (!user) throw new UnauthorizedException('Invalid token');
 
     return user;
