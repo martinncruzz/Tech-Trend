38f4e9a6e304a744325a8f69ee218cb45f63c576
Martín Cruz <martincruz1426@gmail.com>
2025-01-07 20:10:46 -0300

    [api] refactor: simplify UploaderService and improve consistency with imports and adapters

--- api/src/modules/shared/services/uploader/uploader.service.ts
+++ api/src/modules/shared/services/uploader/uploader.service.ts
@@ -1,16 +1,16 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { v2 as cloudinary } from 'cloudinary';
-import streamifier from 'streamifier';
+import { v2 as cloudinary, UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
+import * as streamifier from 'streamifier';
 
-import { MediaOptimizerAdapter } from '../../../../config';
-import { CloudinaryResponse, FOLDER_NAME } from '../..';
+import { FOLDER_NAME } from '@modules/shared/services/uploader/cloudinary/cloudinary.constants';
+import { MediaOptimizerAdapter } from '@config/adapters/media-optimizer.adapter';
 
 @Injectable()
 export class UploaderService {
-  async uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+  async uploadFile(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
     const optimizedBuffer = await MediaOptimizerAdapter.optimizeImage(file);
 
-    return new Promise<CloudinaryResponse>((resolve, reject) => {
+    return new Promise<UploadApiResponse | UploadApiErrorResponse>((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream({ folder: FOLDER_NAME }, (error, result) => {
         if (error) return reject(new InternalServerErrorException('Error uploading image to Cloudinary'));
         resolve(result!);
@@ -20,13 +20,13 @@ export class UploaderService {
     });
   }
 
-  async deleteFile(image_id: string): Promise<void> {
-    const response = await cloudinary.uploader.destroy(image_id);
+  async deleteFile(imageId: string): Promise<void> {
+    const response = await cloudinary.uploader.destroy(imageId);
     if (response.result !== 'ok') throw new InternalServerErrorException('Error deleting image from Cloudinary');
   }
 
-  async updateFile(file: Express.Multer.File, image_id: string): Promise<CloudinaryResponse> {
-    await this.deleteFile(image_id);
+  async updateFile(file: Express.Multer.File, imageId: string): Promise<UploadApiResponse | UploadApiErrorResponse> {
+    await this.deleteFile(imageId);
 
     const updatedFile = await this.uploadFile(file);
 
