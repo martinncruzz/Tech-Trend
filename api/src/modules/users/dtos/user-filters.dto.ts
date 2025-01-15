a7cf1078a3ed9ce51da327b245ae9c796c659765
Martín Cruz <martincruz1426@gmail.com>
2025-01-15 12:37:46 -0300

    [api] feat: add UserFiltersDto

---
+++ api/src/modules/users/dtos/user-filters.dto.ts
@@ -0,0 +1,31 @@
+import { IsEnum, IsOptional, IsString, MinLength, Validate } from 'class-validator';
+
+import { PaginationDto } from '@modules/shared/dtos/pagination.dto';
+import { SortByOrderConstraint } from '@config/constraints/sort-by-order.constraint';
+import { UserRoles } from '@modules/shared/interfaces/enums';
+
+export class UserFiltersDto extends PaginationDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  fullname?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(UserRoles)
+  roles?: UserRoles;
+
+  @IsOptional()
+  @IsEnum(['fullname', 'email', 'createdAt', 'updatedAt'])
+  @Validate(SortByOrderConstraint)
+  sortBy?: 'fullname' | 'email' | 'createdAt' | 'updatedAt';
+
+  @IsOptional()
+  @IsEnum(['asc', 'desc'])
+  @Validate(SortByOrderConstraint)
+  order?: 'asc' | 'desc';
+}
