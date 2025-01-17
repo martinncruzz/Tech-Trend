9b1aeabb4cef698c47a74c5e659df55741b0b6c2
Martín Cruz <martincruz1426@gmail.com>
2025-01-17 16:47:25 -0300

    [api] refactor: update module imports in AppModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,26 +1,22 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from './database';
-import {
-  SharedModule,
-  AuthModule,
-  UsersModule,
-  ProductsModule,
-  CategoriesModule,
-  ShoppingCartsModule,
-  OrdersModule,
-  PaymentsModule,
-} from './modules';
+import { AuthModule } from '@modules/auth/auth.module';
+import { CartsModule } from '@modules/carts/carts.module';
+import { CategoriesModule } from '@modules/categories/categories.module';
+import { OrdersModule } from '@modules/orders/orders.module';
+import { PaymentsModule } from '@modules/payments/payments.module';
+import { ProductsModule } from '@modules/products/products.module';
+import { SharedModule } from '@modules/shared/shared.module';
+import { UsersModule } from '@modules/users/users.module';
 
 @Module({
   imports: [
-    PrismaModule,
     SharedModule,
-    AuthModule,
     UsersModule,
+    AuthModule,
+    CartsModule,
     ProductsModule,
     CategoriesModule,
-    ShoppingCartsModule,
     OrdersModule,
     PaymentsModule,
   ],
