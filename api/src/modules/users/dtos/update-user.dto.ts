cdc84983ce0475201d5ef0efc364a6c0dbf05022
Martín Cruz <martincruz1426@gmail.com>
2025-01-23 15:45:27 -0300

    [api] feat: add password validation rules to UpdateUserDto

--- api/src/modules/users/dtos/update-user.dto.ts
+++ api/src/modules/users/dtos/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsOptional, IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
@@ -9,4 +9,13 @@ export class UpdateUserDto {
   @IsOptional()
   @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'The password must have an uppercase, lowercase letter and a number',
+  })
+  password?: string;
 }
